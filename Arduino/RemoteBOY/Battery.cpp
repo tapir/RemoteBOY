48c612977b4c612af97622f046117500454e153a
Cosku Bas <cosku.bas@gmail.com>
2024-04-02 23:48:02 +0200

    Revert back frequency battery

--- Arduino/RemoteBOY/Battery.cpp
+++ Arduino/RemoteBOY/Battery.cpp
@@ -2,9 +2,9 @@
 #include <Arduino.h>
 
 static const uint8_t  PIN_BATTERY           = A0;
-static const uint32_t BATT_UPDATE_FREQUENCY = 1000; // only update every 20s
-static const float    BATT_MAX_VOLTAGE      = 3.9;  // tests show most of a NiMH battery's charge is inbetween 3.3 - 3.9V
-static const float    BATT_MIN_VOLTAGE      = 3.3;  // https://lygte-info.dk/review/batteries2012/Ikea%20Ladda%20AAA%20900mAh%20%28White%29%20UK.html
+static const uint32_t BATT_UPDATE_FREQUENCY = 20000; // only update every 20s
+static const float    BATT_MAX_VOLTAGE      = 3.9;   // tests show most of a NiMH battery's charge is inbetween 3.3 - 3.9V
+static const float    BATT_MIN_VOLTAGE      = 3.3;   // https://lygte-info.dk/review/batteries2012/Ikea%20Ladda%20AAA%20900mAh%20%28White%29%20UK.html
 
 Battery::Battery(void) { }
 
